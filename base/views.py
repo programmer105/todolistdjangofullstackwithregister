655a19f6dfc24e665a9bb5ccaf35b2f0773bd5fe
Lucas Daniel <lucas.dmdf@bol.com.br>
2022-12-23 17:41:34 -0300

    todolist-frontend

--- base/views.py
+++ base/views.py
@@ -3,11 +3,19 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.urls import reverse_lazy
+
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Task
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+
+# Imports for Reordering Feature
+from django.views import View
+from django.shortcuts import redirect
+from django.db import transaction
+
+from .models import Task
+from .forms import PositionForm
 
 
 class CustomLoginView(LoginView):
@@ -18,6 +26,7 @@ class CustomLoginView(LoginView):
     def get_success_url(self):
         return reverse_lazy('tasks')
 
+
 class RegisterPage(FormView):
     template_name = 'base/register.html'
     form_class = UserCreationForm
@@ -27,7 +36,7 @@ class RegisterPage(FormView):
     def form_valid(self, form):
         user = form.save()
         if user is not None:
-           login(self.request, user) 
+            login(self.request, user)
         return super(RegisterPage, self).form_valid(form)
 
     def get(self, *args, **kwargs):
@@ -35,31 +44,28 @@ class RegisterPage(FormView):
             return redirect('tasks')
         return super(RegisterPage, self).get(*args, **kwargs)
 
-    
-
 
 class TaskList(LoginRequiredMixin, ListView):
-    model = Task 
+    model = Task
     context_object_name = 'tasks'
 
-    def get_context_data(self, **kwargs): 
+    def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        #test
-        #context['color'] = 'red'
         context['tasks'] = context['tasks'].filter(user=self.request.user)
         context['count'] = context['tasks'].filter(complete=False).count()
-        
+
         search_input = self.request.GET.get('search-area') or ''
         if search_input:
             context['tasks'] = context['tasks'].filter(
-                title__startswith= search_input)
+                title__contains=search_input)
 
-        context['search_input']  = search_input  
+        context['search_input'] = search_input
+
         return context
 
 
 class TaskDetail(LoginRequiredMixin, DetailView):
-    model = Task 
+    model = Task
     context_object_name = 'task'
     template_name = 'base/task.html'
 
@@ -73,18 +79,29 @@ class TaskCreate(LoginRequiredMixin, CreateView):
         form.instance.user = self.request.user
         return super(TaskCreate, self).form_valid(form)
 
+
 class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
     fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')
 
+
 class DeleteView(LoginRequiredMixin, DeleteView):
     model = Task
     context_object_name = 'task'
     success_url = reverse_lazy('tasks')
+    def get_queryset(self):
+        owner = self.request.user
+        return self.model.objects.filter(user=owner)
 
+class TaskReorder(View):
+    def post(self, request):
+        form = PositionForm(request.POST)
 
+        if form.is_valid():
+            positionList = form.cleaned_data["position"].split(',')
 
+            with transaction.atomic():
+                self.request.user.set_task_order(positionList)
 
-   
-    
+        return redirect(reverse_lazy('tasks'))
